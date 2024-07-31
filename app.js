bc682eb5d637f6164b3c5091437c041fc2bf0032
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-31 14:51:07 +0300

    js dom added

---
+++ app.js
@@ -0,0 +1,9 @@
+const bgcolor = document.querySelector(".bgDiv");
+const copy = document.querySelector("#copy");
+const colorInput = document.querySelector("#colorInput");
+const colorText = document.querySelector("#colorText");
+const clickBtn = document.querySelector(".btn-click");
+const overBtn = document.querySelector(".btn-over");
+
+
+
