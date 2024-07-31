9235d3baf5207a099e845936ac28ef11c1fc810d
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-31 15:11:11 +0300

    click event added

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ const colorText = document.querySelector("#colorText");
 const clickBtn = document.querySelector(".btn-click");
 const overBtn = document.querySelector(".btn-over");
 
+clickBtn.addEventListener("click", getRandomColorHex);
 
+function getRandomColorHex() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const color = `#${randomColor.padStart(6, "0")}`;
 
+  bgcolor.style.backgroundColor = color;
+}
