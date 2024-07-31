e839e0e4311053b31b54551ec4fc84897a206a47
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-31 15:19:42 +0300

    colorInput added

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const clickBtn = document.querySelector(".btn-click");
 const overBtn = document.querySelector(".btn-over");
 
 clickBtn.addEventListener("click", getRandomColorHex);
+overBtn.addEventListener("mouseover", getRandomColorHex);
 
-function getRandomColorHex() {
+function getRandomColorHex(e) {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   const color = `#${randomColor.padStart(6, "0")}`;
 
   bgcolor.style.backgroundColor = color;
-  colorText.innerHTML = color;
+  colorText.textContent = color;
+  colorInput.value = color;
 }
